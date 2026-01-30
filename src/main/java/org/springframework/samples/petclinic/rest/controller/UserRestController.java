c348a5b645f0ea1d27b51ebcd77c6f59e97b9995
Auto Committer <devnull@example.com>
2026-01-30 11:49:06 +0200

    cleanup

--- src/main/java/org/springframework/samples/petclinic/rest/controller/UserRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/controller/UserRestController.java
@@ -3,8 +3,6 @@ package org.springframework.samples.petclinic.rest.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.mapper.UserMapper;
 import org.springframework.samples.petclinic.model.User;
@@ -12,23 +10,24 @@ import org.springframework.samples.petclinic.rest.dto.UserDto;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.samples.petclinic.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Tag(name = "user", description = "Endpoints related to users.")
+@PreAuthorize("hasRole(@roles.ADMIN)")
 public class UserRestController {
-
     private final UserService userService;
     private final UserMapper userMapper;
 
-    @PreAuthorize( "hasRole(@roles.ADMIN)" )
-    @Operation(operationId = "addUser", summary = "Create a user")
-    @PostMapping(consumes = "application/json")
-    public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto) {
-        HttpHeaders headers = new HttpHeaders();
+    @PostMapping
+    public ResponseEntity<UserDto> addUser(@RequestBody @Validated UserDto userDto) {
         User user = userMapper.toUser(userDto);
         userService.saveUser(user);
-        return new ResponseEntity<>(userMapper.toUserDto(user), headers, HttpStatus.CREATED);
+        return ResponseEntity.created(UriComponentsBuilder.fromPath("/api/users/{username}")
+                .buildAndExpand(user.getUsername()).toUri())
+            .body(userMapper.toUserDto(user));
     }
 }
